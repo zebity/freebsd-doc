d63ecdafc237852b2aa516d9b54eaab3c5d0eab3
John Hartley <zebity@yahoo.com>
2023-09-09 05:39:35 +1000

    Create driver program and add groff/man2html and sgi html handling

--- IRIX/irix-cache-page.sh
+++ IRIX/irix-cache-page.sh
@@ -11,8 +11,16 @@
 #
 #          irix-cache-page.sh -z ZFILE -d CACHE
 #
-#          to build full cache of cat.Zz renders:
-#            find <DIR-WITH-Zz> -type f -name '*.[zZ]' -exec -z {} -d <CACH> \;a
+#          IRIX has 3 diffirent man formats:
+#            zZ - catman files - pre-rendered and compress for ANSI terminal
+#            gzip - sgi supplied HTML pages
+#            man.XX - nroff pages which can be rendered using groff & man2html 
+#
+#          to build full cache of cat.Zz renders, you can use find:
+#            find <DIR-WITH-Zz> -type f -name '*.[zZ]' -exec -z {} -d <CACHE> \;
+#          or
+#            create-irix-man-cache.sh -z -Z -g -Z
+#              where: zZ == cat2man g == gzip html -m == man - build via groff / man2html
 #
 # @note - need to put in alias links as per source dir
 #
@@ -21,15 +29,19 @@
 # (C)opyright 2023 - All rights reserved
 #
 
-USAGE="Usage - ${0} -d cache-dir -z path-to.zZ"
-VALOPTS="d:z:"
+USAGE="Usage - ${0} -d cache-dir -z path-to.(zZ|gz|sec)"
+VALOPTS="c:d:m:p:s:t:z:"
+
 EXPARG=2
 DIR=
 PATH=
 PAGE=
 FILE=
-CATSECTION=
-URL=
+CATSEC=
+MANSEC=
+TYPE=
+URL="/irix-6.5.30/man/\${section}\${subsection}/\${title}"
+ENV_PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 HTML=html
 INDEX=index
 MAN=man
@@ -43,12 +55,20 @@ TMP="lmth2tac.$$."
 BASENAME="/usr/bin/basename"
 CAT2HTML="./irix-catman2html.sh"
 GREP="/usr/bin/grep"
+EGREP="/usr/bin/egrep"
 MKDIR="/bin/mkdir"
 MV="/bin/mv"
 LN="/bin/ln"
 RM="/bin/rm"
 CUT="/usr/bin/cut"
+GZCAT="/usr/bin/gzcat"
+MAN="/usr/bin/man"
+MAN2HTML="/usr/local/bin/man2html"
+SED="/usr/bin/sed"
+ENV="/usr/bin/env"
+SH="/bin/sh"
 
+# DBG
 # echo "DBG>> ${0} - \$#='$#'"
 
 
@@ -70,6 +90,17 @@ do
 	shift 
 done
 
+FILE=`${BASENAME} ${PATH}`
+PATHDIR=${PATH%${FILE}}
+PAGE=${FILE%.*}
+TYPE=${FILE##*.}
+MASK=${PATHDIR%/cat*/*}
+CATSEC=${PATHDIR#${MASK}}
+CATSEC=`echo ${CATSEC} | ${CUT} -f2 -d/`
+MASK=${PATHDIR%/man*/*}
+MANSEC=${PATHDIR#${MASK}}
+MANSEC=`echo ${MANSEC} | ${CUT} -f2 -d/`
+
 if [ ! -d "${DIR}" ]; then
 	${MKDIR} -p "${DIR}/${HTML}"
 	${MKDIR} -p "${DIR}/${INDEX}"
@@ -78,32 +109,54 @@ fi
 if [ -f "${PATH}" ]; then
 	FILE=`${BASENAME} ${PATH}`
 	PATHDIR=${PATH%${FILE}}
-	PAGE=${FILE%[.][zZ]}
-	MASK=${PATHDIR%/cat*/*}
-	CATSECTION=${PATHDIR#${MASK}}
-	CATSECTION=`echo ${CATSECTION} | ${CUT} -f2 -d/`
 else
 	echo "Error - ${0} - cannot find: '$PATH'" 1>&2
 	exit 1
 fi
 
 # DBG
-# echo "DBG>> ${0} - FILE='${FILE}' PATHDIR='${PATHDIR}' PAGE='${PAGE}'"
+# echo "DBG>> ${0} - FILE='${FILE}' PATHDIR='${PATHDIR}' PAGE='${PAGE}' TYPE='${TYPE}' HINT='${PAGE}' CAT='${CATSEC}' MAN='${MANSEC}'."
 
 TMP="${TMP}${PAGE}.html"
 
-${CAT2HTML} -d ${PATHDIR} -s ${CATSECTION} -p ${PAGE} -c ${PAGE} > ${TMP}
+case "${TYPE}" in
+	Z)	;&
+	z)	${CAT2HTML} -d ${PATHDIR} -s ${CATSEC} -p ${PAGE} -c ${PAGE} > ${TMP}
+		;;
+	gz)	${GZCAT} ${PATH} > ${TMP}
+		;;
+	*)	if [ "${MANSEC}" != "" ]; then
+			SEC=${MANSEC#man}
+			# SEC=${SEC%]}
+			#
+			# DBG
+			# echo "DBG>> ${0} - SEC='${SEC}' TYPE='${TYPE}'." 
+			${ENV} PATH=${ENV_PATH} ${SH} ${MAN} ${PATH} | ${MAN2HTML} -cgiurl ${URL} -title "${PAGE}(${TYPE})" > ${TMP}
+		fi
+		;;
+esac
+
 if [ $? -eq 0 ]; then
-	TITLE=`${GREP} '<title>.*</title>' ${TMP}`
+	TITLE=`${EGREP} '<(title|TITLE)>.*</(title|TITLE)>' ${TMP}`
 	if [ "${TITLE}" == "" ]; then
 		${RM} ${TMP}
 		echo "Error - ${0} - Processsing: '${FILE}' - No <Title></Title> tag found in: '${TMP}'." 1>&2
 		exit 1;
+	else
+		TSTSTR=`echo ${TITLE} | ${SED} -e 's/.*>\\(.*\)<\\/.*/\\1/g'`
+		# echo "DBG>> ${0} - TSTSTR='${TSTSTR}'."
+		TITLE=`echo ${TSTSTR} | ${SED} -e 's/\\([A-Za-z][-_\\.a-zA-Z1-9]*\\).*\\([(][1-9a-zA-Z][a-zA-Z1-9]*[)]\\).*/\\1\\2/g'`
+		if [ "${TITLE}" == "" ]; then
+			echo "Error - ${0} - Processsing: '${FILE}' - No <Title></Title> tag found in: '${TMP}'." 1>&2
+			exit 1;
+		fi
 	fi
-	TITLE=${TITLE#<title>}
-	TITLE=${TITLE%</title>}
+	# TITLE=${TITLE#<title>}
+	# TITLE=${TITLE%</title>}
 	# DBG
+	# /
 	# echo "DBG>> ${0} - TITLE='${TITLE}'."
+	# exit 0
 	KEY=${TITLE%%[(]*[)]}
 	SECTION=${TITLE#${KEY}}
 	SECTION=${SECTION#[(]}
